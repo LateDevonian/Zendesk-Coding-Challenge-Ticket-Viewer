cc0587f636767831391d86811b65f673b628391f
LateDevonian <agean27@gmail.com>
2019-06-09 15:28:16 +1000

    updated app and view functionality

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -8,38 +8,62 @@ require_relative './application_controller'
 
 class TicketController < ApplicationController
 
-   get '/tickets' do
+  get '/tickets' do
      @tickets = api_get_tickets
-
      erb :'/tickets/index'
-     # :layout => :main
    end
 
-   get '/tickets/:id' do
-     @ticket = get_ticket_detail
+  get '/tickets/:id' do
+     @ticket = get_ticket_detail(params["id"])
      erb :'tickets/show_each'
-     # :layout => :main
-   end
+  end
+
+  private
+
+  def set_request(url)
+    user = "xx"
+    pass = "yy"
+
+    request = HTTParty.get(url, basic_auth: {username: user,
+    password: pass }, :headers =>{'Content-Type' => 'application/json'} )
+  end
+
 
   def api_get_tickets
-    response = HTTParty.get("url", basic_auth: {username: "usr",
-    password: "password" }, :headers =>{'Content-Type' => 'application/json'} )
+    url = "https://moylen.zendesk.com/api/v2/tickets.json"
+    response = set_request(url)
     if response.success?
        res = response.parsed_response
        t = res["tickets"]
        tickets = t.paginate(:page => params[:page], :per_page => 25)
-       #handle errors gracefully
       else
-        case response.code
-        when 404
-          message = "#{response.code}: Page not found."
-        when 500...600
-          message = "#{response.code}: Oops! "
-        end
+        error_handling(response.code)
     end
   end
 
-  def get_ticket_detail
-    @ticket
+  def get_ticket_detail(id)
+
+    url = "https://moylen.zendesk.com/api/v2/tickets/#{id}.json"
+    response = set_request(url)
+
+    if response.success?
+       res = response.parsed_response
+       t = res["ticket"]
+       #handle errors gracefully
+      else
+        code = response.code
+        error_handling(code)
+    end
+  end
+
+  def error_handling(code)
+    case code
+    when 404
+      message = "#{code}: Page not found."
+    when 500...600
+      message = "#{code}: Oops! "
+    else
+      message = "something is terribly wrong!"
+    end
   end
 end
